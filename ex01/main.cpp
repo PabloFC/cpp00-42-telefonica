242babbcb850d9032a82b0ef9ff535d53ec7fa34
PabloFC <pablofc_68@hotmail.com>
2025-09-03 11:22:46 +0200

    english comments added in main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <pafuente@student.42malaga.com>     +#+  +:+       +#+        */
+/*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 10:30:55 by pafuente          #+#    #+#             */
-/*   Updated: 2025/09/02 21:28:29 by marvin           ###   ########.fr       */
+/*   Updated: 2025/09/03 11:17:36 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main()
 
     while (true)
     {
-        std::cout << "Ingrese comando (ADD, SEARCH, EXIT): ";
+        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
         std::getline(std::cin, command);
 
         if (command == "ADD")
